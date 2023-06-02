68a9dd545e1adbe5997fd6e83da9c09e84806dcf
luotingyang <1661489001@qq.com>
2023-06-02 12:00:06 +0800

    first commit

---
+++ src/main/java/tingyang/luo/git/GitTest.java
@@ -0,0 +1,16 @@
+package tingyang.luo.git;
+
+/**
+ * @Description TODO
+ * @Classname GitTest
+ * @Date 2023/6/2 11:46
+ * @Created by lty
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!!");
+    }
+
+
+}
