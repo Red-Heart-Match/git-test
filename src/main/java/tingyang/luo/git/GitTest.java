b657a0814a8a905ed9ac77890aa11c6e2d97efb1
luotingyang <1661489001@qq.com>
2023-06-02 15:43:54 +0800

    test add branch

--- src/main/java/tingyang/luo/git/GitTest.java
+++ src/main/java/tingyang/luo/git/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
         System.out.println("Hello Git4!!!!");
         System.out.println("push test!!!!");
         System.out.println("pull test!!!!");
+        System.out.println("create new branch test");
     }
 
 
